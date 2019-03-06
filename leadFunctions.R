ea9abd782495746c23b946ba0b76d773956464aa
Marijana Vujkovic <vujkovicm@users.noreply.github.com>
2019-03-06 09:36:58 -0500

    Update leadFunctions.R

--- leadFunctions.R
+++ leadFunctions.R
@@ -7,12 +7,14 @@
 #                                * list of established loci (from literature)
 #                                * plink LD output for all clumped SNPs (from analysis + established)
 #
-#        R functions :  clump.import, snp.block, snp.novel, ld.annotate, snp.annotate, snp.coordinates, overlapping.cojo.snps, updated.primary.lead, blocks.revisited  
+#        R functions :  clump.import, snp.block, snp.novel, ld.annotate, snp.annotate, snp.coordinates, ancestry.tag
+#                          overlapping.cojo.snps, updated.primary.lead, blocks.revisited, clean.cojo, clean.vep
 #
 #            Version :  1.1
 #            Created :  21-Aug-2018
-#           Revision :  fixed bug in coordinates, and added two new functions: overlapping.cojo.snps, blocks.revisited, updated.primary.lead 
-#  Last modification :  20-Feb-2019
+#           Revision :  snp.novel now accounts for missing SNPs in reference plink file, 
+#                       added functions: clean.cojo, clean.vep, ancestry.tag 
+#  Last modification :  6-Mar-2019
 #
 #             Author :  Marijana Vujkovic
 #        Modified by :
@@ -21,10 +23,6 @@
 #            License :  GPL (>=3)
 #
 #=====================================================================================
-
-
-# HLA region hg19
-# chr6:28,477,797-33,448,354
 
 # import clumped file
 clump.import = function(filename = "", pop) {
@@ -54,17 +52,21 @@ snp.block = function(df, df.ld, half.window = 500000, p.threshold = 5e-8) {
   df = df[order(df$CHR, df$BP), ]
   out = NULL
   for (iCHR in 1:22)  {
+     #print(paste("chr", iCHR, sep = ":"))
      df.chr = df[df[, "CHR"] == iCHR & df[, "P"] < p.threshold, c("SNP", "BP", "P", "POP")]
      if (nrow(df.chr) > 0) {
         for (i in 1:nrow(df.chr)) {
+           #print(i)
            current.bp  = df.chr[i, "BP"]
            current.snp = df.chr[i, "SNP"]
            current.df  = df.chr[df.chr[, "BP"] >= current.bp - half.window & df.chr[, "BP"] <= current.bp + half.window, ]
+           # add SNPs that are in R2 with current.bp
            r2.A        = df.ld[which(df.ld$SNP_A == current.snp), "SNP_B"]
            r2.B        = df.ld[which(df.ld$SNP_B == current.snp), "SNP_A"]
            r2          = unique(c(r2.A, r2.B))
            if(length(r2) > 0) {
                 r2.df  = df.chr[which(df.chr$SNP %in% r2),]
+                # merge region based on physical distance and r2
                 current.df  = unique(rbind(current.df, r2.df))
            }
            block.start = min(current.df[, "BP"])
@@ -80,7 +82,8 @@ snp.block = function(df, df.ld, half.window = 500000, p.threshold = 5e-8) {
    }
    out$BLOCK.PRIMARY   = ifelse(out$SNP == out$BLOCK.SNP, 1, 0)
    out$BLOCK.SECONDARY = ifelse(out$SNP != out$BLOCK.SNP, 1, 0)
-   count = 0  # clean up the region id's
+   # clean up the blocks
+   count = 0
    for (i in 1:nrow(out)) {
         if (out$BLOCK.PRIMARY[i] == 1) {
                 count = count + 1
@@ -89,7 +92,8 @@ snp.block = function(df, df.ld, half.window = 500000, p.threshold = 5e-8) {
                 out$BLOCK.N[i] = NA
         }
    }
-   for (i in 2:nrow(out)) {  # merge the blocks that are close
+   # now merge blocks that are knd of close and give them an id
+   for (i in 2:nrow(out)) {
         if (is.na(out$BLOCK.N[i]) == T) {
                 prv = out[which((out$BLOCK.PRIMARY == 1) & (out$BLOCK.N == max(out$BLOCK.N[1:i], na.rm = T))), "SNP"]
                 if(i < nrow(out)) {
@@ -121,7 +125,8 @@ snp.block = function(df, df.ld, half.window = 500000, p.threshold = 5e-8) {
                 }
         }
    }
-   out.final = NULL # now update according to the new block information the positions
+   # update according to the new block information the positions
+   out.final = NULL
    for(i in 1:max(out$BLOCK.N)) {
         block             = out[which(out$BLOCK.N == i), ]
         block$BLOCK.SNP   = block[which(block$BLOCK.P == min(block$BLOCK.P)), "BLOCK.SNP"][[1]] # SNP with minimum P-value
@@ -149,7 +154,8 @@ snp.block = function(df, df.ld, half.window = 500000, p.threshold = 5e-8) {
                 i = i - 1
         }
     }
-    count = 0 # clean up the region id's again
+    # clean up the block ids
+    count = 0
     for (i in 1:nrow(out.final)) {
         if (out.final$BLOCK.PRIMARY[i] == 1) {
                 count = count + 1
@@ -158,7 +164,7 @@ snp.block = function(df, df.ld, half.window = 500000, p.threshold = 5e-8) {
                 out.final$BLOCK.N[i] = NA
         }
     }
-    for (i in 1:nrow(out.final)){ # fill in the missings region id's
+    for (i in 1:nrow(out.final)){ # fill in the missings
         if(is.na(out.final$BLOCK.N[i]) == T) {
                 out.final$BLOCK.N[i] = out.final[which(out.final$SNP == out.final$BLOCK.SNP[i]), "BLOCK.N"]
         }
@@ -166,55 +172,29 @@ snp.block = function(df, df.ld, half.window = 500000, p.threshold = 5e-8) {
     return(out.final)
 }
 
-# is the SNP novel?
-snp.novel = function(df, chr = "CHR", bp = "BP", snp = "BLOCK.SNP", df.ref, df.ld, half.window = 500000) {
-   out = NULL
-   for (iCHR in 1:22) {
-   #print(iCHR)
-   #for(iCHR in 10) {
-      df.chr   = df[df[, chr] == iCHR, ]
-      ref.chr  = df.ref[df.ref[, "CHR"] == iCHR, ]
-      if (nrow(df.chr) > 0) {
-        df.chr$SENTINEL = NA
-        for (i in 1:nrow(df.chr)) {
-           #print(i)
-           if(nrow(ref.chr) == 0) {
-              df.chr$SENTINEL[i] = NA
-           }
-           else {
-              for(j in 1:nrow(ref.chr)) {
-                 #print(j)
-                 if((df.chr[i, "BLOCK.BP"] >= ref.chr[j, bp] - half.window) & (df.chr[i, "BLOCK.BP"] <= ref.chr[j, bp] + half.window)) {
-                    df.chr$SENTINEL[i] = ref.chr[j, "CHRCBP"]
-                 }
-                 else if (df.chr[i, snp] %in% df.ld$SNP_A) {
-                    if(ref.chr[j, "CHRCBP"] %in% df.ld[which(df.ld$SNP_A == df.chr[i, snp]), "SNP_B"]) {
-                       df.chr$SENTINEL[i] = ref.chr[j, "CHRCBP"]
-                    }
-                 }
-                 else if (df.chr[i, snp] %in% df.ld$SNP_B) {
-                    if(ref.chr[j, "CHRCBP"] %in% df.ld[which(df.ld$SNP_B == df.chr[i, snp]), "SNP_A"]) {
-                       df.chr$SENTINEL[i] = ref.chr[j, "CHRCBP"]
-                    }
-                 }
-              }
-           }
-        }
-        out = rbind(out, df.chr)
-      }
-   }
-   out$SENTINEL = ifelse(is.na(out$SENTINEL), "-", out$SENTINEL)
-   out$NOVEL    = ifelse(out$SENTINEL == "-", 1, 0)
-   return(out)
-}
-
-# return list of shared conditionally independent SNPs across blocks (regions) 
+# need to put this as seperate, because need to merge with COJO
 overlapping.cojo.snps = function(df) {
         tmp = unique(df[which(df$SecondarySNP %in% df[duplicated(df$SecondarySNP), "SecondarySNP"] & is.na(df$SecondarySNP) == F), "SecondarySNP"])
         if(length(tmp) == 0) {
                 return(0)
         } else {
                 return(tmp)
+        }
+}
+
+# get the new primary
+updated.primary.lead = function(df) {
+        tmp = unique(df[which(df$SecondarySNP %in% df[duplicated(df$SecondarySNP), "SecondarySNP"] & is.na(df$SecondarySNP) == F), "SecondarySNP"])
+        if(length(tmp) == 0) {
+                return(0)
+        } else {
+                out = NULL
+                for (i in 1:length(tmp)) {
+                        df.tmp    = df[which(df$SecondarySNP == tmp[i]), c("BLOCK.SNP", "BLOCK.P")]
+                        new.lead  = df.tmp[which(df.tmp$BLOCK.P == min(df.tmp$BLOCK.P)), "BLOCK.SNP"]
+                        out       = c(out, new.lead)
+                }
+                return(unique(out))
         }
 }
 
@@ -234,10 +214,37 @@ updated.primary.lead = function(df) {
         }
 }
 
+# clean output from GCTA cojo
+clean.cojo = function(df, prefix = F) {
+        df$LeadSNP   = gsub(pattern = ".jma.cojo*", replacement = "", df$LeadSNP)
+        if(prefix != FALSE) {
+        df$LeadSNP   = gsub(pattern = prefix,       replacement = "", df$LeadSNP)
+        }
+        df = df[order(df$Chr, df$bp), ]
+        for(i in unique(df$LeadSNP)) {
+                if(!(i %in% df$SecondarySNP)) {
+                        # set the minimum SNP to lead SNP
+                        min.p = min(df[which(df$LeadSNP == i), "pJ"])
+                        df[which(df$LeadSNP == i & df$pJ == min.p), "SecondarySNP"] = i
+                }
+        }
+        df = subset(df, df$LeadSNP != df$SecondarySNP)
+        return(df)
+}
+
+# clean VEP output
+clean.vep = function(df) {
+        names(df) = c("SNP", "CHR", "BP", "REA", "CONSEQUENCE", "GENE", "RSID", "SIFT", "POLYPHEN", "AFR_RAF", "AMR_RAF", "EAS_RAF", "EUR_RAF", "SAS_RAF", "AA_RAF", "EA_RAF")
+        df$RSID   = gsub(pattern = ",.*", replacement = "", x = df$RSID)
+        df$SNP    = paste0("chr", df$CHR, ":", df$BP)
+        df$CONSEQUENCE = gsub(pattern = "_VARIANT", replacement = "", x = df$CONSEQUENCE)
+        return(df)
+}
+
 # merge regions which share conditionally independent SNPs
-blocks.revisited = function(df) {
+blocks.revisited = function(df, location = "primary") {
         tmp = overlapping.cojo.snps(df)
-        if(tmp == 0) {
+        if((tmp == 0)[1]) {
                 return(df)
         } else {
                 df.merged  = NULL
@@ -253,15 +260,16 @@ blocks.revisited = function(df) {
                                 df.tmp$COJO.PRIMARY   = ifelse(df.tmp$SNP == df.tmp$COJO.SNP[1], 1, 0)
                                 df.tmp$COJO.START     = df.tmp[which(df.tmp[, "BLOCK.START"] == min(df.tmp[, "BLOCK.START"])), "BLOCK.START"][1]
                                 df.tmp$COJO.END       = df.tmp[which(df.tmp[, "BLOCK.END"] == max(df.tmp[, "BLOCK.END"])), "BLOCK.END"][1]
-                                df.tmp$COJO.N         = min(df.tmp[, "BLOCK.N"])
-                                # set one of the secondary SNPs to missing (in the lesser of the significant lead SNPs)
+                                df.tmp$COJO.N         = min(df.tmp[, "BLOCK.N"]) #hmmm put in the block of the new COJO.SNP
+                                # remove one of the entries
                                 dup.snp = row.names(df.tmp[which((df.tmp$SNP != df.tmp$COJO.SNP) & (df.tmp$SecondarySNP == tmp[i])), ])
+                                # don't remove, just set fields to missing
                                 df.tmp[which(row.names(df.tmp) == dup.snp), c("SecondarySNP", "bp", "refA", "freq", "b", "se", "p", "n", "freq_geno", "bJ", "bJ_se", "pJ", "LD_r")] = rep(NA, 13)
                                 df.merged = rbind(df.merged, df.tmp)
                                 block.list = c(block.list,  old.blocks)
                         }
                         else {
-                                # remove the duplicate entry from df.merged
+                                # remove another duplicate entry from df.merged
                                 dup.snp = row.names(df.merged[which((df.merged$SNP != df.merged$COJO.SNP) & (df.merged$SecondarySNP == tmp[i])), ])
                                 df.merged = df.merged[which(row.names(df.merged) != dup.snp), ]
                         }
@@ -319,35 +327,78 @@ ld.annotate = function(df.ld, df.ref, df.trans = F, df.eur = F, df.sas = F, df.a
    return(df.ld)
 }
 
-
-# check if the variants from race-specific meta-analysis are in the overall-transethnic meta-analyisis
-# if not, they are distinct variants
-snp.distinct = function(df, df.ref = trans, snp.name = "SNP", pop = "EUR", ref = "TRANS") {
-   pop.A = paste0(pop, "_A")
-   pop.B = paste0(pop, "_B")
-   flag.distinct = paste0(pop, ".distinct")
-   for(i in 1:nrow(df)) {
-      if (df[i, snp.name] %in% df.ref[, snp.name]){
-         df$TRANS.SENTINEL[i] = df[i, snp.name]
+# is the SNP novel?
+snp.novel = function(df, chr = "CHR", bp = "BP", snp = "BLOCK.SNP", df.ref, df.ld, half.window = 500000, missing.ref.snps = F) {
+   out = NULL
+   for (iCHR in 1:22) {
+   #print(iCHR)
+      df.chr   = df[df[, chr] == iCHR, ]
+      ref.chr  = df.ref[df.ref[, "CHR"] == iCHR, ]
+      if (is.data.frame(missing.ref.snps)){
+            missing.chr = subset(missing.ref.snps, missing.ref.snps$CHR == iCHR)
       }
-      else {
-         if ((df[i, snp.name] %in% ld$SNP_A) && (ld[which(ld$SNP_A == df[i, snp.name]), pop.A] == 1) && (ld[which(ld$SNP_A == df[i, snp.name]), "TRANS_B"] == 1)) {
-            df$TRANS.SENTINEL[i] = ld[which(ld$SNP_A == df[i, snp.name]), "SNP_B"]
-         }
-         else {
-            if ((df[i, snp.name] %in% ld$SNP_B) && (ld[which(ld$SNP_B == df[i, snp.name]), pop.B] == 1) && (ld[which(ld$SNP_B == df[i, snp.name]), "TRANS_A"] == 1)) {
-               df$TRANS.SENTINEL[i] = ld[which(ld$SNP_B == df$CHRCBP[i]), "SNP_A"]
-            }
-            else {
-               df$TRANS.SENTINEL[i] = NA
-            }
-         }
-       }
-    }
-    df[, flag.distinct] = ifelse(is.na(df$TRANS.SENTINEL) == T, 1, 0)
-    return(df)
+      if (nrow(df.chr) > 0) {
+        df.chr$SENTINEL = NA
+        for (i in 1:nrow(df.chr)) {
+           #print(i)
+           if(nrow(ref.chr) == 0) {
+              df.chr$SENTINEL[i] = NA
+           }
+           else {
+              for(j in 1:nrow(ref.chr)) {
+                 #print(j)
+                 if((df.chr[i, "BLOCK.BP"] >= ref.chr[j, bp] - half.window) & (df.chr[i, "BLOCK.BP"] <= ref.chr[j, bp] + half.window)) {
+                    df.chr$SENTINEL[i] = ref.chr[j, "CHRCBP"]
+                 }
+                 else if (df.chr[i, snp] %in% df.ld$SNP_A) {
+                    if(ref.chr[j, "CHRCBP"] %in% df.ld[which(df.ld$SNP_A == df.chr[i, snp]), "SNP_B"]) {
+                       df.chr$SENTINEL[i] = ref.chr[j, "CHRCBP"]
+                    }
+                 }
+                 else if (df.chr[i, snp] %in% df.ld$SNP_B) {
+                    if(ref.chr[j, "CHRCBP"] %in% df.ld[which(df.ld$SNP_B == df.chr[i, snp]), "SNP_A"]) {
+                       df.chr$SENTINEL[i] = ref.chr[j, "CHRCBP"]
+                    }
+                 }
+             }
+             if (is.data.frame(missing.ref.snps)){
+                if(nrow(missing.chr) > 0) {
+                   for(j in 1:nrow(missing.chr)) {
+                      if((df.chr[i, "BLOCK.BP"] >= missing.chr[j, bp] - half.window) & (df.chr[i, "BLOCK.BP"] <= missing.chr[j, bp] + half.window & (is.na(df.chr$SENTINEL[i]) == T))) {
+                          df.chr$SENTINEL[i] = missing.chr[j, "CHRCBP"]
+                      }
+                   }
+                }
+             }
+           }
+        }
+        out = rbind(out, df.chr)
+      }
+   }
+   out$SENTINEL = ifelse(is.na(out$SENTINEL), "-", out$SENTINEL)
+   out$NOVEL    = ifelse(out$SENTINEL == "-", 1, 0)
+   return(out)
 }
 
+# ancestry tag for transethnic meta-analysis
+tag.ancestry = function(ld.trans, df.eur = F, df.sas = F, df.amr = F, df.eas = F, r2.high = 0.8, r2.low = 0.05, exact.high.combined = T) {
+        if(data.frame(df.eur)) { ld.eur  = data.frame(SNP = unique(df.eur[, c("SNP")]), EUR = 1, AFR = 0, SAS = 0, AMR = 0, EAS = 0) } else { ld.eur = data.frame(SNP=NULL, EUR=NULL) }
+        if(data.frame(df.amr)) { ld.amr  = data.frame(SNP = unique(df.amr[, c("SNP")]), EUR = 0, AFR = 0, SAS = 0, AMR = 1, EAS = 0) }
+        if(data.frame(df.sas)) { ld.sas  = data.frame(SNP = unique(df.sas[, c("SNP")]), EUR = 0, AFR = 0, SAS = 1, AMR = 0, EAS = 0) }
+        if(data.frame(df.eas)) { ld.eas  = data.frame(SNP = unique(df.eas[, c("SNP")]), EUR = 0, AFR = 0, SAS = 0, AMR = 0, EAS = 1) }
+        if(data.frame(df.afr)) { ld.afr  = data.frame(SNP = unique(df.afr[, c("SNP")]), EUR = 0, AFR = 1, SAS = 0, AMR = 0, EAS = 0) }
+        # gotta make sure that LD trans always has EAS AMR, even if it means they are all 0
+        ld.tmp1 = ld.trans[,c("SNP_A", "EUR", "AFR", "SAS", "AMR", "EAS")]
+        ld.tmp2 = ld.trans[,c("SNP_B", "EUR", "AFR", "SAS", "AMR", "EAS")]
+        names(ld.tmp1)[1] = names(ld.tmp2)[1] = "SNP"
+        ld.tmp = unique(rbind(ld.tmp1, ld.tmp2, ld.eur, ld.amr, ld.sas, ld.eas, ld.afr))
+        ld.full = ddply(ld.tmp, "SNP", numcolwise(sum))
+        ld.tmp = ld.full[,2:6]
+        ld.tmp[ld.tmp > 1] = 1
+        ld.full = cbind(ld.full$SNP, ld.tmp)
+        names(ld.full)[1] = "SNP"
+        ld.full$AncestryTag = paste(ld.full$EUR, ld.full$AFR, ld.full$SAS, ld.full$AMR, ld.full$EAS, sep=".")
+}
 
 # get nearest gene for each SNP
 # requires dataframe that contains at least chromosome (1) and basepair location (57865)
